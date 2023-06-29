2c1d7068b95b90277f1b2b5d804e7d9d4b3f3c30
Juebele <eronuebele@gmail.com>
2023-06-29 09:31:29 -0400

    set up flex on the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,20 @@ import '../App.css';
 
 const Header = () => {
     return (
-        <div>
-            <p>This is the Header doc</p>
+        <div class='container'>
+            <div>
+                <img src='../Assets/hangar-logo.png'></img>
+            </div>
+            <div>
+                <p>Welcome to my site!</p>
+            </div>
+            <div>
+                <ul>
+                    <li>Link 1</li>
+                    <li>Link 2</li>
+                    <li>Link 3</li>
+                </ul>
+            </div>
         </div>
     )
 }
