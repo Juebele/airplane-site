9d2e737c6f61b3f610500ef3aed72bfd948a8fc9
Juebele <eronuebele@gmail.com>
2023-06-29 10:55:37 -0400

    added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import '../App.css';
 const Footer = () => {
     return (
         <div>
-            <p>This is the Footer doc</p>
+            <p id='footer'>Copyright 2023 James Uebele</p>
         </div>
     )
 }
